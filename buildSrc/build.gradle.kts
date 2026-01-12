5430b3f67292ec176ca0b1e3eb187573d322ff15
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-01-12 20:02:13 +0300

    init gradle

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+repositories.mavenCentral()
+
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    implementation("com.github.kepocnhh:Gradlex:0.1.0")
+}
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,10 @@
+object Version {
+    const val jvmTarget = "17"
+    const val kotlin = "2.2.21"
+
+    object Android {
+        const val compileSdk = 36
+        const val minSdk = 24
+        const val targetSdk = compileSdk
+    }
+}
