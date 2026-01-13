ddd637e2f39db1f32055b1e5be95a082453ee89d
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-01-13 14:48:47 +0300

    wip

--- android/src/main/kotlin/org/kepocnhh/swapper/App.kt
+++ android/src/main/kotlin/org/kepocnhh/swapper/App.kt
@@ -1,12 +1,19 @@
 package org.kepocnhh.swapper
 
 import android.app.Application
+import org.kepocnhh.swapper.provider.FinalLoggers
 import org.kepocnhh.swapper.provider.Injection
+import org.kepocnhh.swapper.provider.Loggers
 
 internal class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        // todo
+        val loggers: Loggers = FinalLoggers
+        val logger = loggers.create("[App]")
+        logger.debug("create")
+        _injection = Injection(
+            loggers = loggers,
+        )
     }
 
     companion object {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "Swapper"
 
-include("app")
+include("android")
