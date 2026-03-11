e0eeb0d09db8d82c143812d06fff5f8d91f41cf1
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-11 21:11:53 +0300

    desktop

--- desktop/src/main/kotlin/org/kepocnhh/swapper/App.kt
+++ desktop/src/main/kotlin/org/kepocnhh/swapper/App.kt
@@ -1,28 +1,32 @@
 package org.kepocnhh.swapper
 
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.kepocnhh.swapper.module.main.MainScreen
 import org.kepocnhh.swapper.provider.FinalLoggers
 import org.kepocnhh.swapper.provider.FinalTimes
 import org.kepocnhh.swapper.provider.Injection
 import org.kepocnhh.swapper.provider.Loggers
 import org.kepocnhh.swapper.provider.Times
 
+internal object App {
+    private var _injection: Injection? = null
+    val injection: Injection get() = checkNotNull(_injection) { "No injection!" }
+
+    init {
+        val loggers: Loggers = FinalLoggers
+        val times: Times = FinalTimes()
+        _injection = Injection(
+            loggers = loggers,
+            times = times,
+        )
+    }
+}
+
 fun main() {
-    val loggers: Loggers = FinalLoggers
-    val times: Times = FinalTimes()
-    val injection = Injection(
-        loggers = loggers,
-        times = times,
-    )
     application {
         Window(onCloseRequest = ::exitApplication) {
-            val logger = remember { injection.loggers.create("[App]") }
-            LaunchedEffect(Unit) {
-                logger.debug("now: ${injection.times.now()}")
-            }
+            MainScreen()
         }
     }
 }
